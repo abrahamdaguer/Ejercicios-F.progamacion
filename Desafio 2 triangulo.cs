dce176b1c44691377da084ad42cc6be0f611fc6e
abrahamdaguer <68035021+abrahamdaguer@users.noreply.github.com>
2020-07-12 20:50:04 -0500

    Add files via upload

---
+++ Desafio 2 triangulo.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace desafio_2_triangulo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ejercicio 2");
+            Console.Write("Ingrese T: ");
+            double t = double.Parse(Console.ReadLine());
+
+            Console.Write("Ingrese a: ");
+            double a = double.Parse(Console.ReadLine());
+
+            //conversion de grados a radianes
+            double aRad = a * Math.PI / 180;
+
+            // calcular los Catetos
+            double y = t * Math.Sin(aRad);
+            
+            double z = (t * Math.Cos(aRad));
+
+            // Valor angulos 
+            double c = 90 - a;
+
+            Console.WriteLine(" El valor del lado y es: " + y);
+            
+            Console.WriteLine("El valor de lado z es: " + z);
+            
+            Console.WriteLine("El ángulo interno C es: " + c);
+        }
+    }
+}
---
+++ Desafio 3 triangulo.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace ejercicio_3_triangulo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Input del usuario
+            Console.WriteLine("ejercicio 3");
+            Console.Write("Ingrese Z: ");
+            double z = double.Parse(Console.ReadLine());
+
+            Console.Write("Ingrese c: ");
+            double c = double.Parse(Console.ReadLine());
+
+           
+            // calular Angulos
+            double a = 90 - c;
+            
+            
+            //Conversion de grados a  radianes
+            double cRad = c * Math.PI / 180;
+            double aRad = a * Math.PI / 180;
+            
+            
+            //hallar catetos y la hipotenusaa
+            double y = (z * Math.Sin(aRad)) / (Math.Sin(cRad));
+            double t = (y) / (Math.Sin(aRad));
+
+
+            //consola shows
+            Console.WriteLine("El valor de la Hipotenusa: " + t);
+            Console.WriteLine("El valo de y: " + y);
+            Console.WriteLine("El ángulo interno es: " + a);
+        }
+    }
+}
