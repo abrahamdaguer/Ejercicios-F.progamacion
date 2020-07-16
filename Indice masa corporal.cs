9b12e64978447a2aa499b265585039c4d83f8562
abrahamdaguer <68035021+abrahamdaguer@users.noreply.github.com>
2020-07-16 16:16:02 -0500

    Update Indice masa corporal.cs

--- Indice masa corporal.cs
+++ Indice masa corporal.cs
@@ -19,27 +19,40 @@ namespace Imc
 
             Imc = peso / ((altura/100) * (altura/100));
 
-            if (Imc <= 15)
+            if (Imc <= 15){
             
                 Console.WriteLine("Delgadez muy severa:" + Imc);
-            else if (15 <= Imc && Imc <= 15.9)
+            }
+            else if (15 <= Imc && Imc <= 15.9) {
                 Console.WriteLine("Delgadez severa:" + Imc);
-                  else if (16 <= Imc && Imc <= 18.4)
-                          Console.WriteLine("Delgadez: " + Imc);
-                      else if (18.5 <= Imc && Imc <= 24.9)
-                                Console.WriteLine("Peso saludable:" + Imc);
-                           else if (25 <= Imc && Imc <= 29.9)
+            }
+          
+            else if (16 <= Imc && Imc <= 18.4)
+            {
+              Console.WriteLine("Delgadez: " + Imc);
+            }
+           
+            else if (18.5 <= Imc && Imc <= 24.9)
+            {
+              Console.WriteLine("Peso saludable:" + Imc);
+            }
+           
+            else if (25 <= Imc && Imc <= 29.9){
+                
 
-                                    Console.WriteLine("Sobrepeso:" + Imc);
-                                 else if (30 <= Imc && Imc <= 34.4)
+            Console.WriteLine("Sobrepeso:" + Imc);
+            }
+            
+            else if (30 <= Imc && Imc <= 34.4) {
 
-                                            Console.WriteLine("obvesidad severa:" + Imc);
+            Console.WriteLine("obvesidad severa:" + Imc);
+            }
+           
+            else {
 
-                                      else (Imc >= 40)
+             Console.WriteLine("obvesidad mórbida:" + Imc);
 
-                                             Console.WriteLine("obvesidad mórbida:" + Imc);
-
-
+            }
             Console.ReadKey();
 
 
