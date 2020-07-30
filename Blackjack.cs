2e2d2895804536cf54d7fcc09d33392c2a05ab0d
abrahamdaguer <68035021+abrahamdaguer@users.noreply.github.com>
2020-07-30 11:48:53 -0500

    Add files via upload

---
+++ Blackjack.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+namespace Blackjack
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            int carta = 2, total = 0, puntos = 0;
+            Random aleatorio = new Random();
+            string continuar = "Si"; 
+            Console.WriteLine("Desea Iniciar el juego (si/no)");
+            continuar = Console.ReadLine();
+            puntos = aleatorio.Next(2, 21);
+            total = puntos;
+            Console.WriteLine("\nCartas: " + carta + " | Valor de las cartas: " + total );
+
+            while (continuar == "si")
+            {
+                puntos = aleatorio.Next(1, 11);
+                Console.WriteLine("\nDesea continuar (si/no)");
+                continuar  = Console.ReadLine();
+                if (continuar == "si")
+                {
+                    carta = carta + 1;
+                    total += puntos;
+                    Console.WriteLine("\nCartas:" + carta + " | Valor de la carta " + puntos + "| Total:" + total );
+                }
+               
+                
+                if (total > 21)
+                {
+                    total = 0;
+                    Console.WriteLine("\n\nSu total fue mayor a 21 y ha sido eliminado");
+                    continuar = "no";
+
+                }
+                else if (total == 21)
+                {
+                    Console.WriteLine("\nFelicidades ha ganado");
+                    continuar = "no";
+                }
+                
+               
+            }
+                Console.Write(" Gracias por jugar  ");
+                Console.Write("\nSu total fue: " + total);
+            
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Valores extremos .cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace Valores_Extremos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double total = 0;
+            Console.WriteLine("Ingrese el numero de datos ");
+            int datos = int.Parse(Console.ReadLine());
+            int contador = 0, max = 0, min = 200;
+            string nombreMayor = "nombreMayor";
+            string nombreMenor = "nombreMenor";
+            while (contador < datos)
+            {
+                Console.WriteLine("Ingrese el nombre:");
+                string nombre = Console.ReadLine();
+                Console.WriteLine("Edad:");
+                int edad = int.Parse(Console.ReadLine());
+
+                if (edad > max)
+                {
+                    max = edad;
+                    nombreMayor = nombre;
+
+
+
+                }
+                if (edad < min)
+                {
+                    min = edad;
+                    nombreMenor = nombre;
+
+                }
+                total += edad;
+                contador++;
+
+            }
+            double promedio = total / datos;
+            Console.WriteLine("\n Promedio:" + promedio + "| Mayor" + max + "| Menor:" + min);
+            Console.WriteLine("\nNombre Mayor:" + nombreMayor);
+            Console.WriteLine("\nNombre Menor:" + nombreMenor);
+
+            Console.ReadKey();
+        }
+    }
+}
